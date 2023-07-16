b5083ab360e680104925e5def5b575e975d88330
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-16 12:04:30 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,10 @@
+/* Explicit Typecasting... */
+class A 
+{
+    public static void main(String[] args) {
+        
+        double a=10.5;  // 8 Bytes
+        int b=(int)a;  // 4 Bytes
+        System.out.print(b);                   // IN OUTPUT NO DECIMAL PLACE
+    }
+}
